575b6f4ff7b3ff3fd3a1868dbbfffa9faaf70db5
Alex'Sea <82589219+alexisR83@users.noreply.github.com>
2021-10-20 11:39:08 +0200

    moteurs dans io

--- Projet_Failbot.X/IO.h
+++ Projet_Failbot.X/IO.h
@@ -9,15 +9,24 @@
 // Prototypes fonctions
 void InitIO();
 
-// Définitions des pins pour les hacheurs moteurs
+//Définitions des moteurs
 #define MOTEUR1_IN1 _LATB14
 #define MOTEUR1_IN2 _LATB15
+#define MOTEUR2_IN1 _LATB12
+#define MOTEUR2_IN2 _LATB13
 
-// Configuration éspcifique du moteur gauche
+//Configuration spécifique du moteur gauche
 #define MOTEUR_GAUCHE_H_IO_OUTPUT MOTEUR1_IN1
 #define MOTEUR_GAUCHE_L_IO_OUTPUT MOTEUR1_IN2
-#define MOTEUR_GAUCHE_L_PWM_ENABLE IOCON1bits .PENL
-#define MOTEUR_GAUCHE_H_PWM_ENABLE IOCON1bits .PENH
+#define MOTEUR_GAUCHE_L_PWM_ENABLE IOCON1bits.PENL
+#define MOTEUR_GAUCHE_H_PWM_ENABLE IOCON1bits.PENH
 #define MOTEUR_GAUCHE_DUTY_CYCLE PDC1
+
+//Configuration spécifique du moteur droit
+#define MOTEUR_DROIT_H_IO_OUTPUT MOTEUR2_IN1
+#define MOTEUR_DROIT_L_IO_OUTPUT MOTEUR2_IN2
+#define MOTEUR_DROIT_L_PWM_ENABLE IOCON6bits.PENL
+#define MOTEUR_DROIT_H_PWM_ENABLE IOCON6bits.PENH
+#define MOTEUR_DROIT_DUTY_CYCLE PDC6
 
 #endif /* IO_H */
